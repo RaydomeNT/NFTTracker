0ca6b8f011c247a955b9b83108be434c31f7c2b8
RaydomeNT <raydome85@gmail.com>
2022-11-09 13:19:56 +0000

    email and password validators added

--- backend/src/user/model.js
+++ backend/src/user/model.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require('bcrypt');
+const validator = require('validator');
 
 const Schema = mongoose.Schema
 
@@ -22,6 +23,17 @@ const userSchema = new Schema({
 
 //static signup method
 userSchema.statics.signup = async function(username, email, password) {
+
+  //validation
+  if (!email || !password) {
+    throw Error('All fields must be filled')
+  }
+  if (!validator.isEmail(email)){
+    throw Error('Email is not valid')
+  }
+  if (!validator.isStrongPassword(password)){
+    throw Error('Password not strong enough')
+  }
   
   const userExists = await this.findOne({ username })
 
