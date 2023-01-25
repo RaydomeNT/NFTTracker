021f7ef69ba12ef9918ced024ae9a4813ffae6c5
RaydomeNT <raydome85@gmail.com>
2023-01-25 13:34:18 +0000

    Login and Logut hooks, auth status

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,15 @@
 import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Navbar = () => {
+  const { logout } = useLogout()
+  const { user } = useAuthContext()
+
+  const handleClick = () => {
+    logout()
+  }
+
   return (
     <header>
       <div className="container">
@@ -9,10 +18,18 @@ const Navbar = () => {
         <Link to="/projects"><h1>NFT Projects</h1></Link>
         <Link to="/twitter"><h1>NFT Twitter</h1></Link>
       <nav>
-        <div>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Signup</Link>
-        </div>
+        {user && (
+          <div>
+            <span className="userDisplay">{user.email}</span>
+            <button onClick={handleClick}>Log out</button>
+          </div>
+        )}
+        {!user && (
+          <div>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Signup</Link>
+          </div>
+        )}
       </nav>
       </div>
     </header>
