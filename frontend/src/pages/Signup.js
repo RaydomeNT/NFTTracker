c61537dfde06dc1d78cada81b64cfc2ca0f96cee
RaydomeNT <raydome85@gmail.com>
2023-01-25 12:22:09 +0000

    Signup auth working, connected to db, bugs fixed

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,19 +1,28 @@
 import { useState } from 'react'
+import { useSignup } from '../hooks/useSignup'
 
 const Signup = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [username, setUsername] = useState('');
+    const {signup, isLoading, error} = useSignup();
 
     const handleSubmit = async (e) => {
         e.preventDefault()
 
-        console.log(email, password)
+        await signup(username, email, password)
     }
 
     return (
         <form className = "signup" onSubmit={handleSubmit}>
             <h3>Sign up</h3>
 
+            <label>User Name:</label>
+            <input
+            type="text"
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+            />
             <label>Email:</label>
             <input
             type="email"
@@ -26,7 +35,8 @@ const Signup = () => {
             onChange={(e) => setPassword(e.target.value)}
             value={password}
             />
-            <button>Sign up</button>
+            <button disabled={isLoading}>Sign up</button>
+            {error && <div className="error">{error}</div>}
         </form>
     )
 }
